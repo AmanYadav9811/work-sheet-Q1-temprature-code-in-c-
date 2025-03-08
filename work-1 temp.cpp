759b216d9fa2805fb49312b5a8f395d2c1c60ae6
AmanYadav9811 <168656984+AmanYadav9811@users.noreply.github.com>
2025-03-08 17:42:34 +0545

    Update work-1 temp.cpp

--- work-1 temp.cpp
+++ work-1 temp.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main() {
-    double temperature;
+    double temp;
     char scale;
 
 
@@ -11,24 +11,31 @@ int main() {
     while (!(cin >> temperature)) {
 
         cin.clear();
-        cin.ignore(numeric_limits<streamsize>::max(), '\n');
-        cout << "Invalid input. Please enter a numeric value: ";
+        cin.ignore(numeric_limits <streamsize>::max(), <<endl;)
+        cout << "Invalid input. 
+            Please enter a numeric value: ";
     }
 
 
-    cout << "Choose the conversion scale (C for Celsius, F for Fahrenheit): ";
+    cout << "Choose the conversion scale
+        (C for Celsius, F for Fahrenheit): ";
     cin >> scale;
 
 
-    if (scale == 'C' || scale == 'c') {
+    if (scale == 'C' || scale == 'c') 
+    {
 
         double fahrenheit = (temperature * 9 / 5) + 32;
         cout << "Converted temperature: " << fahrenheit << " F" << endl;
-    } else if (scale == 'F' || scale == 'f') {
+        
+    }
+    else if (scale == 'F' || scale == 'f') {
 
         double celsius = (temperature - 32) * 5 / 9;
         cout << "Converted temperature: " << celsius << " C" << endl;
-    } else {
+    } 
+    else
+    {
 
         cout << "Invalid scale selected. Please choose 'C' or 'F'." << endl;
     }
